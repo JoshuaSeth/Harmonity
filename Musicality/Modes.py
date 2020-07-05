591cd03a86664c703793d14c5083b65ebc12f78d
JoshuaSeth <SB@accette.eu>
2020-07-05 11:54:53 +0200

    Added Mode

--- Musicality/Modes.py
+++ Musicality/Modes.py
@@ -1,7 +1,9 @@
 
 Modes = {"Major" : [2,2,1,2,2,2,1],
-"Lydian" : [2,2,2,1,2,2,1],
-"Harmonic_Minor" : [2,1,2,2,1,3,1],
-"Phrygian_Dominant" : [1,3,1,2,1,2,2],
-         "Natural_Minor" : [2,1,2,2,1,2,2],
-         "Melodic_Minor" : [2,1,2,2,2,2,1]}
+            "Lydian" : [2,2,2,1,2,2,1],
+            "Mixolydian" : [2,2,1,2,2,1,2],
+            "Harmonic_Minor" : [2,1,2,2,1,3,1],
+            "Phrygian_Dominant" : [1,3,1,2,1,2,2],
+            "Natural_Minor" : [2,1,2,2,1,2,2],
+            "Melodic_Minor" : [2,1,2,2,2,2,1],
+         }
